82d1c613027974ad1142c9ae8f29d187b436dcb4
Sofistication <sordonez93@gmail.com>
2017-03-14 14:44:55 -0400

    Respond to challenge
    - most of this solution is the result of a google search
    - I think I understand what is going on here, though I am unsure
    - how I would reduce the number of lines.
    - I would also appreciate some insight on `#send`
    - I think I understand its usage here but don't think I quite understand
    - what other potential uses it could have.

--- lib/challenge.rb
+++ lib/challenge.rb
@@ -1,4 +1,21 @@
 # frozen_string_literal: true
 def calc(expr)
-  # Your code here
+  return 0 if expr.length.zero?
+  stack = []
+  until expr.empty?
+    case expr
+    when /\A-?\d+(\.\d+)?/
+      stack.push($&.to_f)
+    when /\S*/
+      second_operand = stack.pop
+      first_operand = stack.pop
+
+      stack.push(first_operand.send($&, second_operand))
+    end
+
+    expr = $'
+    expr.lstrip!
+  end
+
+  stack.pop
 end
